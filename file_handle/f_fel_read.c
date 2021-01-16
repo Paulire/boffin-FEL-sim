5be552969430e68361abe448d116af5b0bb1725b
Paul <paulire@btinternet.com>
2021-01-16 16:03:29 +0000

    edited file input read

--- file_handle/f_fel_read.c
+++ file_handle/f_fel_read.c
@@ -12,36 +12,35 @@ double* read_from_config( char *name )
 {
 	// File varlibles
 	char ch;
-	//char buff_arg[20];	// buffers for the command and the name
-	//char buff_num[20];
+	char buff_arg[20];	// buffers for the command and the name
+	char buff_num[20];
 	int is_arg = 1;
 	FILE *fp;
 	
 	// Open file then error if NULL file
 	fp = fopen(name, "r");
-
 	if( fp == NULL) { _err_(5); }
 	
 	// Data verlibles
 	
 	// Read file
-	while( (ch=fgetc(fp) ) != EOF) {
-		if( ch != '\n' ) {
-			if (ch != ' ')  {
-////////////////////////
-if( is_arg == 1) {
-	if( ch == '=') { is_arg = 0; 
-	} else if( ch == ';') { _err_(7); }
-} else {
-	if( ch == ';') { is_arg = 1; 
-	} else if( ch == '=') { _err_(6); }
-}
-
-///////////////////////
+	for( int i=0; (ch=fgetc(fp) ) != EOF; i++ ) {
+		if (ch != ' ')  {
+			if( is_arg == 1) {
+				if( ch == '=') { is_arg = 0; i = -1; buff_arg[i] = '\0'; 
+				} else if( ch == ';') { _err_(7);
+				} else { buff_arg[i] = ch; }
+			} else {
+				if( ch == ';') { is_arg = 1; i = -1; buff_num[i] = '\0'; break;
+				} else if( ch == '=') { _err_(6);
+				} else { buff_num[i] = ch; }
 			}
+
+
 		}
-	}
-	printf("\n");
+	} else { i--; }
+	
+
 	fclose(fp);
 
 	return 0;
