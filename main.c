06850cad7eae2311403b3e06cd103186ac3496a1
Paul <paulire@btinternet.com>
2021-01-17 13:08:28 +0000

    More of before

--- main.c
+++ main.c
@@ -3,7 +3,10 @@
 
 // Loading shared libries 
 #include "file_handle/file_handle.h"
+#ifndef FEL_INTERGRATOR_H
+#define FEL_INTERGRATOR_H
 #include "inter/fel_intergrator.h"
+#endif
 
 #ifndef FEL_INPUT_STRUC_H
 #define FEL_INPUT_STRUC_H
@@ -36,18 +39,20 @@ int main( int argc, char *argv[])
 	int ELECTRON_NUM = fel_input_data.N_theta*fel_input_data.N_p;
 	float *z_vals = (float*)malloc( fel_input_data.z_num*sizeof(float) );
 	float *phi_vals = (float*)malloc( fel_input_data.z_num*sizeof(float) );
+	float *a_vals = (float*)malloc( fel_input_data.z_num*sizeof(float) );
 
 
 	// Sets values for integration data
+	a_vals[0] = fel_input_data.a_0;
 	phi_vals[0] = fel_input_data.phi_0;
 
-	for( int i=0; i<fel_input_data.z_num; i++)
+	for( int i=0; i<=fel_input_data.z_num; i++)
 	{
 		z_vals[i] = i*(fel_input_data.z_f)/fel_input_data.z_num;
 	}
 
 
-	printf("%f\n", z_vals[50]);
+	printf("%f\n", z_vals[100]);
 
 	// Integrator
 	call_me("Inter\n");
@@ -55,6 +60,7 @@ int main( int argc, char *argv[])
 	// Return memory for input data
 	free(z_vals);
 	free(phi_vals);
+	free(a_vals);
 
 	return 0;
 }
