38acbe73e8261bcdd9a8ea6836bbf076e646878e
Paul <paulire@btinternet.com>
2021-01-30 18:37:35 +0000

    Fixed conceraction of enery issue

--- boffin/fel_intergrator.c
+++ boffin/fel_intergrator.c
@@ -6,10 +6,9 @@
 #include <math.h>
 #endif
 
-#include <gsl/gsl_odeiv2.h>
-#ifndef GSL_GSL_odeiv2_H
-#define GSL_GSL_odeiv2_H
-//#include <gsl/gsl_odeiv.h>
+#ifndef GSL_GSL_odeiv_H
+#define GSL_GSL_odeiv_H
+#include <gsl/gsl_odeiv.h>
 #endif
 
 #ifndef GSL_GSL_MATH_H
@@ -33,7 +32,7 @@
 /////////////////////////////////////////////
 
 // There are some summations of sin and cos which get called from here
-static inline void sum_inter( double *restrict out, double *restrict in, int ELEC_NUM )
+static inline void sum_inter( double *restrict out, const double *restrict in, int ELEC_NUM )
 {
 	for( int i=0; i<ELEC_NUM; i++ ) {
 		out[0] += (double)cos( in[ 2+i ] + in[1] );
@@ -50,9 +49,7 @@ static inline void sum_inter( double *restrict out, double *restrict in, int ELE
 static inline int fel_ode( double x, const double y[], double f[], register void *params )
 {
 	int ELEC_NUM = *( int*)params;
-	double temp;
-	double temp2;
-double cos_sin[2] = {0,0};
+	double cos_sin[2] = {0,0};
 	sum_inter( cos_sin, y, ELEC_NUM );
 
 	// Sets the integral for each p and theta value
@@ -61,13 +58,7 @@ double cos_sin[2] = {0,0};
 		int t_i_val = 2+i;
 		f[ t_i_val ] = y[ p_i_val ];											// dthetadz = p
 		f[ p_i_val ] = -2*y[ 0 ]*cos( y[ t_i_val ] + y[1] );		// dpdz = -2a*cos( theta + phi )
-		temp = y[p_i_val];
-		temp2 = y[t_i_val];
 	}
-
-	// Now for p and a
-	//double cos_sin[2];
-	//sum_inter( cos_sin, y, ELEC_NUM );
 	
 	f[0] = (double)cos_sin[0];
 	f[1] = (double)(-1/y[0])*(double)cos_sin[1];
@@ -80,8 +71,8 @@ double cos_sin[2] = {0,0};
 static inline int fel_jac( double x, const double y[], double *dfdy, double dfdt[], void *params)
 {
 	register int ELEC_NUM = *(int *)params;
-	// double out[2];
-double cos_sin[2] = {0,0};
+	double cos_sin[2] = {0,0};
+
 	sum_inter( cos_sin, y, ELEC_NUM );
 
 	gsl_matrix_view dfdy_mat = gsl_matrix_view_array (dfdy, ELEC_NUM*2+2, 4);
@@ -116,15 +107,12 @@ double cos_sin[2] = {0,0};
 
 void boffin_solve( double *restrict z_data, double **restrict fel_data_matrix, int ELECTRON_NUM, int Z_NUM )
 {
-	//const gsl_odeiv2_step_type * T = /*gsl_odeiv2_step_bsimp;*/ gsl_odeiv2_step_rk8pd;
-	/*const gsl_odeiv2_step_type * T = gsl_odeiv2_step_msbdf;
-	gsl_odeiv2_step * s = gsl_odeiv2_step_alloc( T, 2+2*ELECTRON_NUM );
-	gsl_odeiv2_control * c = gsl_odeiv2_control_y_new( 1e-6, 0 );
-	gsl_odeiv2_evolve * e = gsl_odeiv2_evolve_alloc( 2+2*ELECTRON_NUM );
-	gsl_odeiv2_driver * d gsl_odeiv2_driver_all*/
+	const gsl_odeiv_step_type * T = gsl_odeiv_step_gear2;
+	gsl_odeiv_step * s = gsl_odeiv_step_alloc( T, 2+2*ELECTRON_NUM );
+	gsl_odeiv_control * c = gsl_odeiv_control_y_new( 1e-8, 1e-8 );
+	gsl_odeiv_evolve * e = gsl_odeiv_evolve_alloc( 2+2*ELECTRON_NUM );
 
-	gsl_odeiv2_system sys = { fel_ode, fel_jac, 2+2*ELECTRON_NUM, &ELECTRON_NUM };
-	gsl_odeiv2_driver * d = gsl_odeiv2_driver_alloc_y_new( &sys, gsl_odeiv2_step_msadams, 1e-3, 1e-6, 1e-8);
+	gsl_odeiv_system sys = { fel_ode, fel_jac, 2+2*ELECTRON_NUM, &ELECTRON_NUM };
 	
 	double *y = (double*) malloc( (2*ELECTRON_NUM+2)*sizeof(double));
 
@@ -139,37 +127,9 @@ void boffin_solve( double *restrict z_data, double **restrict fel_data_matrix, i
 		}
 
 		while( z_i < z_step ) {
-			//int status = gsl_odeiv2_evolve_apply ( e, c, s, &sys, &z_i, z_step, &h, y );
-			int status = gsl_odeiv2_driver_apply( d, &z_i, z_step , y);
+			int status = gsl_odeiv_evolve_apply ( e, c, s, &sys, &z_i, z_step, &h, y );
 
 			if( status != GSL_SUCCESS ) { break; }
-
-			if ( z_i  > 0.2155e-1 ) {
-				//d = gsl_odeiv2_driver_alloc_y_new( &sys, gsl_odeiv2_step_msbdf, 1e-3, 1e-6, 1e-8);
-				printf("\t bdf \n");
-			}
-			if ( z_i > 0.72878e1 )
-			{	
-				d = gsl_odeiv2_driver_alloc_y_new( &sys, gsl_odeiv2_step_msadams, 1e-3, 1e-6,1e-8);
-				//T = gsl_odeiv2_step_msadams;
-				//T = gsl_odeiv2_step_bsimp;
-				//gsl_odeiv2_step_reset( s );
-				//s = gsl_odeiv2_step_alloc( T, 2+2*ELECTRON_NUM );
-				printf("\t adm \n");
-
-			}
-			if ( z_i  > 11 ) {
-				//d = gsl_odeiv2_driver_alloc_y_new( &sys, gsl_odeiv2_step_msbdf, 1e-3, 1e-6, 1e-8);
-			}
-			/*if ( z_i > 8.0 )
-			{
-				//T = gsl_odeiv2_step_gear1;
-				T = gsl_odeiv2_step_gear2;
-				gsl_odeiv2_step_reset( s );
-				s = gsl_odeiv2_step_alloc( T, 2+2*ELECTRON_NUM );
-			}*/
-			printf("%lf\n", z_i);
-
 		}
 
 		for( int e=0; e<( 2*ELECTRON_NUM+2 ); e++ ) {
@@ -178,7 +138,6 @@ void boffin_solve( double *restrict z_data, double **restrict fel_data_matrix, i
 	}
 
 	free(y);
-	//gsl_odeiv2_step_free( s );
-	//gsl_odeiv2_control_free( c );
-	gsl_odeiv2_driver_free(d);
+	gsl_odeiv_step_free( s );
+	gsl_odeiv_control_free( c );
 }
